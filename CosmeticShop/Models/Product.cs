08727516b0810e103464f6c3a0871b8b5ed242dc
stValentyn <valik7km@gmail.com>
2021-06-27 02:56:38 +0300

    Add Product class

---
+++ CosmeticShop/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CosmeticShop.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string ShortName { get; set; }
+        public string FullName { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+
+        //picture
+
+        public string IdGroup { get; set; }
+        public string IdCategory { get; set; }
+
+    }
+}
